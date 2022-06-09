538b7c44d6046bd864b938cbce5ef981ef2bb45c
Carl Johnson <me@carlmjohnson.net>
2022-06-08 21:31:15 -0400

    Validator: Rename methods to Add, AddIf, AddIfUnset

--- validator_test.go
+++ validator_test.go
@@ -12,8 +12,8 @@ import (
 
 func ExampleValidator() {
 	var v resperr.Validator
-	v.Ensure("heads", 2 < 1, "Two are better than one.")
-	v.Ensure("heads", !true, "I win, tails you lose.")
+	v.AddIf("heads", 2 > 1, "Two are better than one.")
+	v.AddIf("heads", true, "I win, tails you lose.")
 	err := v.Err()
 
 	fmt.Println(resperr.StatusCode(err))
@@ -28,15 +28,15 @@ func ExampleValidator() {
 	// heads = I win, tails you lose.
 }
 
-func ExampleValidator_EnsureIf() {
+func ExampleValidator_AddIfUnset() {
 	var v resperr.Validator
 	x, err := strconv.Atoi("hello")
-	v.Ensure("x", err == nil, "Could not parse x.")
-	v.Ensure("x", x > 0, "X must be positive.")
+	v.AddIf("x", err != nil, "Could not parse x.")
+	v.AddIf("x", x < 1, "X must be positive.")
 
 	y, err := strconv.Atoi("hello")
-	v.Ensure("y", err == nil, "Could not parse y.")
-	v.EnsureIf("y", y > 0, "Y must be positive.")
+	v.AddIf("y", err != nil, "Could not parse y.")
+	v.AddIfUnset("y", y < 1, "Y must be positive.")
 	fmt.Println(v.Err())
 	// Output:
 	// validation error: x=Could not parse x. x=X must be positive. y=Could not parse y.
@@ -44,8 +44,8 @@ func ExampleValidator_EnsureIf() {
 
 func TestValidator(t *testing.T) {
 	var v1 resperr.Validator
-	v1.Ensure("heads", 2 < 1, "Two are better than one.")
-	v1.Ensure("heads", !true, "I win, tails you lose.")
+	v1.AddIf("heads", 2 > 1, "Two are better than one.")
+	v1.AddIf("heads", true, "I win, tails you lose.")
 	err := v1.Err()
 	be.Nonzero(t, err)
 	be.False(t, v1.Valid())
@@ -55,8 +55,8 @@ func TestValidator(t *testing.T) {
 	be.Equal(t, http.StatusBadRequest, resperr.StatusCode(err))
 
 	var v2 resperr.Validator
-	v2.Ensure("heads", 2 > 1, "One is the loneliest number.")
-	v2.Ensure("heads", true, "I win, tails you lose.")
+	v2.AddIf("heads", 2 < 1, "One is the loneliest number.")
+	v2.AddIf("heads", false, "I win, tails you lose.")
 	err = v2.Err()
 	be.True(t, v2.Valid())
 	be.NilErr(t, err)
@@ -66,7 +66,7 @@ func TestValidator(t *testing.T) {
 	// Don't allocate for valid messages
 	allocs := testing.AllocsPerRun(10, func() {
 		var v resperr.Validator
-		v.Ensure("field", true, "message: %d", 1)
+		v.AddIf("field", false, "message: %d", 1)
 		err = v.Err()
 	})
 	be.Equal(t, 0, allocs)
